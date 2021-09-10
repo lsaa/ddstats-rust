e3f2eb722b10a1751058219b08cb4aff2b9c9835
lsaa <lsaadev.personal@gmail.com>
2021-09-10 07:31:37 -0300

    junk zip pathes

--- .github/workflows/package.sh
+++ .github/workflows/package.sh
@@ -1,5 +1,5 @@
 sudo apt-get install zip
 echo "# Automated Release from Github Action" > body.md
 cp default_cfg.ron config.ron
-zip ddstats-rust-linux-x86_64.zip target/x86_64-unknown-linux-gnu/release/ddstats-rust config.ron
-zip ddstats-rust-windows-x86_64.zip target/x86_64-pc-windows-gnu/release/ddstats-rust.exe config.ron
+zip -j ddstats-rust-linux-x86_64.zip target/x86_64-unknown-linux-gnu/release/ddstats-rust config.ron
+zip -j ddstats-rust-windows-x86_64.zip target/x86_64-pc-windows-gnu/release/ddstats-rust.exe config.ron
