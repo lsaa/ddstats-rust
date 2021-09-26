5482a14825f01d41243f073139d7f972acde6320
lsaa <lsaadev.personal@gmail.com>
2021-09-25 22:41:06 -0300

    new stuff

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -6,5 +6,25 @@ sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
 rm -f $PROTOC_ZIP
 
+set -x
+
+wget --no-check-certificate https://xcb.freedesktop.org/dist/xcb-proto-1.11.tar.bz2
+wget --no-check-certificate https://xcb.freedesktop.org/dist/libxcb-1.11.1.tar.bz2
+
+tar -xjf xcb-proto-1.11.tar.bz2
+cd xcb-proto-1.11
+./configure
+make
+sudo make install
+cd ..
+
+tar -xjf libxcb-1.11.1.tar.bz2
+cd libxcb-1.11.1
+./configure --enable-xkb --enable-xinput --enable-selinux
+make
+sudo make install
+cd ..
+
+sudo apt-get install libx11-xcb-dev libgl1-mesa-dev
 sudo apt install xorg-dev
 sudo apt install openssl libssl-dev pkg-config
