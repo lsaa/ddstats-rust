a37eedeb84f1ad1d88b4e965a06d47a7804f6168
lsaa <lsaadev.personal@gmail.com>
2022-02-19 23:48:20 -0300

    pre v2

--- linux_install.sh
+++ linux_install.sh
@@ -1,4 +1,2 @@
+sudo setcap cap_sys_ptrace=eip ddstats-rust
 sudo install -Dm755 ddstats-rust /usr/bin/ddstats-rust
-sudo setcap cap_sys_ptrace=eip /usr/bin/ddstats-rust
-mkdir -p ${XDG_CONFIG_HOME:-~/.config}/ddstats-rust/
-cp config.ron ${XDG_CONFIG_HOME:-~/.config}/ddstats-rust/
